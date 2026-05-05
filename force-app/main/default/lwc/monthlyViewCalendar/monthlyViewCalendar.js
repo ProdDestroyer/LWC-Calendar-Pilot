c79890b291027fef8468ccc979ecae08d0d88666
ProdDestroyer <samuelfelipelopez@gmail.com>
2026-05-04 23:11:49 -0500

    -Calendar Events Rendered in Monthly View
    Next Actions:
    - Handle isLoading in eventsCalendar (Project Parent Component)
    
    - Start Daily View calendar events rendering development

--- force-app/main/default/lwc/monthlyViewCalendar/monthlyViewCalendar.js
+++ force-app/main/default/lwc/monthlyViewCalendar/monthlyViewCalendar.js
@@ -1,35 +1,97 @@
 import { LightningElement, api } from 'lwc';
-import { WEEK_DAYS_NAMES, ARE_DATES_EQUAL } from 'c/utils';
+import { WEEK_DAYS_NAMES, ARE_DATES_EQUAL, PRINT_ERROR } from 'c/utils';
 import getCalendarEvents from '@salesforce/apex/EventsCalendarController.getCalendarEvents'
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';    
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class MonthlyViewCalendar extends LightningElement {
+
+    firstTimeConnected = true;
     _pivotDate;
+    lastRequestIndex = 0;
     currentMonthTiles = [];
     isLoading = true;
     calendarEvents = [];
+    calendarEventsMap = {};
 
     async connectedCallback() {
-        this.buildCurrentMonthViewTiles();
+        if (this.firstTimeConnected) {
+            this.firstTimeConnected = false;
+        }
+        await this.reloadData();
+    }
+
+    async reloadData() {
+        this.lastRequestIndex += 1;
+        this.isLoading = true;
         await this.retrieveCalendarEvents();
+        this.buildCurrentMonthViewTiles();
         this.isLoading = false;
     }
 
     async retrieveCalendarEvents() {
+        const lastRequestIndex = this.lastRequestIndex;
+        const { firstDayOfArray, lastDayOfArray } = this.buildMonthBaseData();
         const { calendarEventsWrapper: { calendarEvents, timeZone }, errorMessage, isError } = await getCalendarEvents({
-            startDate: this.currentMonthTiles[0].date,
-            endDate: this.currentMonthTiles[this.currentMonthTiles.length - 1].date,
+            startDate: firstDayOfArray,
+            endDate: lastDayOfArray,
         });
+
+        if(this.lastRequestIndex != lastRequestIndex) return;
 
         if (isError) {
             showToast('Error', errorMessage, 'error');
         } else {
+
             this.calendarEvents = calendarEvents;
+            this.calendarEventsMap = {};
+
+            calendarEvents.forEach(calendarEvent => {
+                const startTime = this.buildCurrentUserTime(new Date(calendarEvent.Start_Time__c), timeZone);
+                const endTime = this.buildCurrentUserTime(new Date(calendarEvent.End_Time__c), timeZone);
+                const key = `${startTime.month}-${startTime.day}`;
+                const eventType = calendarEvent.Type__c;
+
+                this.calendarEventsMap[key] ??= [];
+                this.calendarEventsMap[key].push({
+                    startTime,
+                    endTime,
+                    title: calendarEvent.Title__c,
+                    type: eventType,
+                    class: (eventType == 'Health') ? 'calendar-event-box green' : (eventType == 'Social') ? 'calendar-event-box yellow' : 'calendar-event-box red',
+                    id: calendarEvent.Id,
+                });
+            });
         }
     }
 
     buildCurrentMonthViewTiles() {
+        const { totalTilesAmount, firstDayOfMonthWeekIndex, pastAndCurrentMonthTilesAmount, firstDayOfArray } = this.buildMonthBaseData();
+
         const targetMonthTiles = [];
+        try {
+            for (let i = 0; i < totalTilesAmount; i++) {
+                const currentDate = new Date(firstDayOfArray);
+                currentDate.setDate(currentDate.getDate() + i);
+                const grayedOut = (i < firstDayOfMonthWeekIndex || i > pastAndCurrentMonthTilesAmount - 1);
+                const key = `${((currentDate.getMonth() + 1 < 10) ? '0' : '') + (currentDate.getMonth() + 1)}-${((currentDate.getDate() < 10) ? '0' : '') + currentDate.getDate()}`;
+                const tileCalendarEvents = this.calendarEventsMap[key] || [];
+
+                targetMonthTiles.push({
+                    date: currentDate,
+                    dateNumber: currentDate.getDate(),
+                    dateString: currentDate.toDateString(),
+                    className: !grayedOut ? (ARE_DATES_EQUAL(currentDate, new Date())) ? 'date-tile today-background' : 'date-tile' : 'date-tile grayed-out',
+                    calendarEvents: tileCalendarEvents,
+                    truncateEvents: tileCalendarEvents.length > 3,
+                });
+            }
+        } catch (error) {
+            PRINT_ERROR(error);
+        }
+        this.currentMonthTiles = targetMonthTiles;
+    }
+
+    buildMonthBaseData() {
         const pivotDate = this.pivotDate;
         const firstDayOfMonth = new Date(pivotDate.getFullYear(), pivotDate.getMonth(), 1);
         const firstDayOfMonthWeekIndex = firstDayOfMonth.getDay();
@@ -41,20 +103,19 @@ export default class MonthlyViewCalendar extends LightningElement {
         const pastAndCurrentMonthTilesAmount = firstDayOfMonthWeekIndex + daysAmountInMonth;
         const futureTilesAmount = (7 - (pastAndCurrentMonthTilesAmount % 7)) % 7;
         const totalTilesAmount = pastAndCurrentMonthTilesAmount + futureTilesAmount;
-
-        for (let i = 0; i < totalTilesAmount; i++) {
-            const currentDate = new Date(firstDayOfArray);
-            currentDate.setDate(currentDate.getDate() + i);
-            const grayedOut = (i < firstDayOfMonthWeekIndex || i > pastAndCurrentMonthTilesAmount - 1);
-            targetMonthTiles.push({
-                date: currentDate,
-                dateNumber: currentDate.getDate(),
-                dateString: currentDate.toDateString(),
-                className: !grayedOut ? (ARE_DATES_EQUAL(currentDate, new Date())) ? 'date-tile today-background' : 'date-tile' : 'date-tile grayed-out',
-            });
-        }
-
-        this.currentMonthTiles = targetMonthTiles;
+        const lastDayOfArray = new Date(firstDayOfArray);
+        lastDayOfArray.setDate(lastDayOfArray.getDate() + totalTilesAmount - 1);
+        return {
+            pivotDate,
+            firstDayOfMonth,
+            firstDayOfMonthWeekIndex,
+            firstDayOfArray,
+            lastDayOfArray,
+            daysAmountInMonth,
+            pastAndCurrentMonthTilesAmount,
+            futureTilesAmount,
+            totalTilesAmount,
+        };
     }
 
     buildCurrentUserTime(date, timeZone) {
@@ -63,6 +124,7 @@ export default class MonthlyViewCalendar extends LightningElement {
             year: 'numeric',
             month: '2-digit',
             day: '2-digit',
+            weekday: 'long',
             hour: '2-digit',
             minute: '2-digit',
             second: '2-digit',
@@ -98,7 +160,9 @@ export default class MonthlyViewCalendar extends LightningElement {
 
     set pivotDate(newPivotDate) {
         this._pivotDate = newPivotDate;
-        this.buildCurrentMonthViewTiles();
+        if (!this.firstTimeConnected) {
+            this.reloadData();
+        }
     }
 
     get weekDaysNames() {
--- force-app/main/default/lwc/utils/utils.js
+++ force-app/main/default/lwc/utils/utils.js
@@ -1,17 +1,22 @@
-export const  WEEK_DAYS_NAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-export const  MONTHS_NAMES = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+export const WEEK_DAYS_NAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+export const MONTHS_NAMES = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 export const BUILD_DAY_HOURS_BLOCKS = () => {
-        const dailyViewHourBlocks = [];
-        for (let i = 0; i < 24; i++) {
-            const suffix = (i < 13) ? 'am' : 'pm';
-            const formattedHour = (i % 12) + (12 * (Math.trunc(i / 12)));
-            dailyViewHourBlocks.push({ hour: i, hourString: `${ ((i < 10) ? '0': '') + formattedHour}:00${suffix}` });
-        }
-        return dailyViewHourBlocks;
+    const dailyViewHourBlocks = [];
+    for (let i = 0; i < 24; i++) {
+        const suffix = (i < 13) ? 'am' : 'pm';
+        const formattedHour = (i % 12) + (12 * (Math.trunc(i / 12)));
+        dailyViewHourBlocks.push({ hour: i, hourString: `${((i < 10) ? '0' : '') + formattedHour}:00${suffix}` });
     }
+    return dailyViewHourBlocks;
+}
 
-export const ARE_DATES_EQUAL = (firstDate, secondDate) =>  {
+export const ARE_DATES_EQUAL = (firstDate, secondDate) => {
     return (firstDate.getFullYear() == secondDate.getFullYear() &&
-            firstDate.getMonth() == secondDate.getMonth() &&
-             firstDate.getDate() == secondDate.getDate());
+        firstDate.getMonth() == secondDate.getMonth() &&
+        firstDate.getDate() == secondDate.getDate());
+}
+
+export const PRINT_ERROR = (exceptionError) => {
+    console.error(exceptionError.stack);
+    console.error(exceptionError.message);
 }
